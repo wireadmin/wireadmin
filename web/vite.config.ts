ee2f5808b89d82bfc83a5bedf300e2206b424e06
Shahrad Elahi <shahradq@gmail.com>
2023-12-11 03:43:42 +0330

    update deps

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
 
-export default defineConfig({
+export default {
   plugins: [ sveltekit() ],
   test: {
     include: [ 'src/**/*.{test,spec}.{js,ts}' ]
   }
-});
+};
